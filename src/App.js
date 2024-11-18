0e5c337238f3dce0e94ec78d49dd017e9b327529
TaigaTi <tarikabirch@gmail.com>
2024-11-18 16:24:46 -0400

    Added user sequence

--- src/App.js
+++ src/App.js
@@ -1,44 +1,70 @@
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVolumeMute } from '@fortawesome/free-solid-svg-icons';
-import { useEffect, useState } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faVolumeMute } from '@fortawesome/free-solid-svg-icons';
+import { useCallback, useEffect, useState } from 'react';
 
 const tiles = ["tile-1", "tile-2", "tile-3", "tile-4"]
 
 function App() {
   const [sequence, setSequence] = useState([]);
+  const [userSequence, setUserSequence] = useState([]);
+  const [userPlayed, setUserPlayed] = useState(false);
 
-  const addColor = () => {
+  const addColor = useCallback(() => {
     const randomIndex = Math.floor(Math.random() * tiles.length);
     const nextTile = tiles[randomIndex];
 
     setSequence((prevSequence) => [...prevSequence, nextTile])
-    console.log(sequence)
-  }
+  }, [setSequence]);
 
-  const playSequence = () => {
+  const playSequence = useCallback(() => {
     sequence.forEach((tile, index) => {
       let currentTile = document.getElementById(tile);
-  
-      const delay = index * 3000;
-  
+
+      const delay = index * 2000;
+
       setTimeout(() => {
         currentTile.classList.add(tile + "-active");
       }, delay);
-  
+
       setTimeout(() => {
         currentTile.classList.remove(tile + "-active");
-      }, delay + 1500); 
+      }, delay + 800);
     });
-  };
+  }, [sequence]);
+
+  const addUserSequence = (tile) => {
+    setUserSequence((prevSequence) => [...prevSequence, tile]);
+  }
+
+  const resetGame = () => {
+    setSequence([]);
+    setUserSequence([]);
+    setUserPlayed(false);
+    setTimeout(addColor, 1000);
+  }
 
   useEffect(() => {
-    addColor()
-    addColor()
-    addColor()
-    addColor()
-    setTimeout(() => { playSequence() }, 10000);
-  }, []);
+    if (sequence.length === 0) {
+      addColor();
+      setUserPlayed(true);
+    }
+  }, [sequence, addColor]);
+
+  useEffect(() => {
+    if (userSequence.length === sequence.length) {
+      setUserPlayed(true);
+      addColor();
+      setUserSequence([]);
+    }
+  }, [userSequence, sequence, addColor]);
+
+  useEffect(() => {
+    if (sequence.length > 0 && userPlayed) {
+      setUserPlayed(false);
+      setTimeout(playSequence, 1000);
+    }
+  }, [userPlayed, sequence, addColor, playSequence]);
 
   return (
     <div className="App">
@@ -50,24 +76,24 @@ function App() {
         <div className="controls">
           <p className="score">Score: 0</p>
           <p className="audio">
-            <FontAwesomeIcon icon={faVolumeMute} size="2x" />
+            {/* <FontAwesomeIcon icon={faVolumeMute} size="2x" /> */}
           </p>
         </div>
 
         <div className="board">
           <div className="board-row">
-            <button id="tile-1" className="tile"></button>
-            <button id="tile-2" className="tile"></button>
+            <button id="tile-1" className="tile" onClick={() => addUserSequence("tile-1")}></button>
+            <button id="tile-2" className="tile" onClick={() => addUserSequence("tile-2")}></button>
           </div>
 
           <div className="board-row">
-            <button id="tile-3" className="tile"></button>
-            <button id="tile-4" className="tile"></button>
+            <button id="tile-3" className="tile" onClick={() => addUserSequence("tile-3")}></button>
+            <button id="tile-4" className="tile" onClick={() => addUserSequence("tile-4")}></button>
           </div>
         </div>
 
         <div className="reset-control">
-          <button className="reset-button">
+          <button className="reset-button" onClick={resetGame}>
             Reset
           </button>
         </div>
@@ -77,3 +103,4 @@ function App() {
 }
 
 export default App;
+
